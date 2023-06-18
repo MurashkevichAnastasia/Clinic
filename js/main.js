86bc7b969a4b6371961323ca769f645353de4064
A M <nstymur@gmail.com>
2023-06-18 20:18:55 +0300

    img + js

---
+++ js/main.js
@@ -0,0 +1,78 @@
+//HAMBURGER
+let headerHamburgerMenuBtn = document.querySelector(".header-hamburger-menu-btn");
+let headerHamburgerMenu = document.querySelector(".header-hamburger-menu")
+
+headerHamburgerMenuBtn.addEventListener("click", function(){
+  headerHamburgerMenuBtn.classList.toggle("active");
+  headerHamburgerMenu.classList.toggle("active");
+})
+
+
+
+
+
+
+// MEDIA. HOVER ON BEHANCE
+function hoverOnBehance() {
+  document.getElementById("behance-subtittle-arrow").classList.add("active");
+}
+function hoverOffBehance() {
+  document.getElementById("behance-subtittle-arrow").classList.remove("active");
+}
+// MEDIA. HOVER ON BEHANCE ARROW
+function hoverOnArrow() {
+  document.getElementById("black-arrow").classList.add("active");
+}
+function hoverOffArrow() {
+  document.getElementById("black-arrow").classList.remove("active");
+}
+
+
+// MEDIA. HOVER ON GITHUB
+function hoverOnGithub() {
+  document.getElementById("github-subtittle-arrow").classList.add("active");
+}
+function hoverOffGithub() {
+  document.getElementById("github-subtittle-arrow").classList.remove("active");
+}
+// MEDIA. HOVER ON GITHUB ARROW
+function hoverOnArrowGit() {
+  document.getElementById("black-arrow-git").classList.add("active");
+}
+function hoverOffArrowGit() {
+  document.getElementById("black-arrow-git").classList.remove("active");
+}
+
+
+// MEDIA. HOVER ON TELEGRAM
+function hoverOnTelegram() {
+  document.getElementById("telegram-subtittle-arrow").classList.add("active");
+}
+function hoverOffTelegram() {
+  document.getElementById("telegram-subtittle-arrow").classList.remove("active");
+}
+// MEDIA. HOVER ON TELEGRAM ARROW
+function hoverOnArrowTelegram() {
+  document.getElementById("black-arrow-telegram").classList.add("active");
+}
+function hoverOffArrowTelegram() {
+  document.getElementById("black-arrow-telegram").classList.remove("active");
+}
+
+
+// MEDIA. HOVER ON TWITTER
+function hoverOnTwitter() {
+  document.getElementById("twitter-subtittle-arrow").classList.add("active");
+}
+function hoverOffTwitter() {
+  document.getElementById("twitter-subtittle-arrow").classList.remove("active");
+}
+// MEDIA. HOVER ON TWITTER ARROW
+function hoverOnArrowTwitter() {
+  document.getElementById("black-arrow-twitter").classList.add("active");
+}
+function hoverOffArrowTwitter() {
+  document.getElementById("black-arrow-twitter").classList.remove("active");
+}
+
+
